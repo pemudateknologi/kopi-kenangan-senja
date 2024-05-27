87f59f56c0c5000719f5d026c350f065da9c3e99
pemudateknologi <achferdihamdani@gmail.com>
2024-05-27 22:23:26 +0700

    menambahkan slide keranjang dan menambahkan tombol keranjang di card dan menambahkan footer

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const links = document.querySelector(".links");
 menu.addEventListener("click", function () {
   links.classList.toggle("active");
 });
+
+const keranjang = document.getElementById("tombol-keranjang");
+const menuKeranjang = document.getElementsByClassName("menu-keranjang");
+
+keranjang.addEventListener("click", function () {
+  menuKeranjang[0].classList.toggle("active");
+});
